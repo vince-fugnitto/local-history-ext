cb04c4defc56ea9372cd12895f957463c8821a00
Kaiyue Pan <kaiyue.pan@ericsson.com>
2020-04-14 17:39:44 -0400

    Load history files on activation and code refactor
    - Load all local history file entries on activation
    - Refactor code for generating timestamp
    
    Signed-off-by: Kaiyue Pan <kaiyue.pan@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -2,26 +2,42 @@ import * as vscode from 'vscode';
 import { LocalHistoryProvider } from './local-history';
 
 export function activate(context: vscode.ExtensionContext) {
+    const disposable: vscode.Disposable[] = [];
+
+    // Initialize a local history provider and load entries.
     const provider = new LocalHistoryProvider();
+    if (vscode.workspace.workspaceFolders) {
+        provider.loadEntriesOnActivation();
+    }
 
-    let disposable = vscode.commands.registerCommand('local-history.echo', () => {
-        vscode.window.showInformationMessage('\'Local History\' is active.');
+    vscode.workspace.onDidChangeWorkspaceFolders(() => {
+        vscode.window.showInformationMessage('The workspace has been updated!');
+        if (vscode.workspace.workspaceFolders) {
+            provider.loadEntriesOnActivation();
+        }
     });
-    context.subscriptions.push(disposable);
 
-    context.subscriptions.push(vscode.commands.registerTextEditorCommand('local-history.viewAllForActiveEditor',
-        () => {
+    // Simple command to echo if the extension has successfully loaded and is active.
+    disposable.push(vscode.commands.registerCommand('local-history.echo', () => {
+        vscode.window.showInformationMessage('\'Local History\' is active.');
+    }));
+
+    // Command which lists the local history for the active editor.
+    disposable.push(
+        vscode.commands.registerTextEditorCommand('local-history.viewAllForActiveEditor', () => {
             const editor = vscode.window.activeTextEditor;
             if (editor) {
                 provider.viewAllForActiveEditor(editor);
             }
-        }
-    ));
+        })
+    );
 
-    // Create history on save document
+    // Listen to editor save events to create local history files.
     vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
         provider.saveActiveEditorContext(document);
     });
+
+    context.subscriptions.push(...disposable);
 }
 
 export function deactivate() { }
