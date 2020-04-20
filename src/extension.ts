3218c38b2712c97f41ea74265653f58d973c24cb
Kaiyue Pan <kaiyue.pan@ericsson.com>
2020-04-20 08:44:00 -0400

    Revert active editor to its previous state
    Add a `Clock icon` in the top right menu for diff mode
    When user click the Clock icon, Display pop-up message asking the user to confirm the reversion
    If Revert is selected, the active editor will be reverted to its previous revision
    
    Signed-off-by: Kaiyue Pan <kaiyue.pan@ericsson.com>

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 import { LocalHistoryManager } from './local-history/local-history-manager';
 
 export function activate(context: vscode.ExtensionContext) {
@@ -13,6 +14,19 @@ export function activate(context: vscode.ExtensionContext) {
         // Displays the local history for the currently active editor.
         vscode.commands.registerTextEditorCommand('local-history.viewAllForActiveEditor', () => {
             provider.viewAllForActiveEditor(vscode.window.activeTextEditor!);
+        })
+    );
+
+    disposable.push(
+        vscode.commands.registerTextEditorCommand('local-history.revertActiveEditorToPrevRevision', () => {
+            const editors = vscode.window.visibleTextEditors;
+            if (editors && editors.length === 2) {
+                vscode.window.showWarningMessage(`Are you sure you want to revert '${path.basename(editors[1].document.fileName)}' to its previous state?`, { modal: true }, 'Revert').then((selection) => {
+                    if (selection === 'Revert') {
+                        provider.revertActiveEditorToPrevRevision(editors[0], editors[1]);
+                    }
+                });
+            }
         })
     );
 
